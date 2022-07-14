fdbb26d2d67b17a8b9d4b92bd0d384dd19add892
Vivek <dongarevivek81@gmail.com>
2022-07-14 15:41:08 +0530

    version 2

--- src/main/java/com/pack/Script.java
+++ src/main/java/com/pack/Script.java
@@ -19,8 +19,8 @@ import java.nio.file.Paths;
 
 public class Script {
 
-	static FirefoxOptions addSpecifications(FirefoxOptions options) {
-		
+    static FirefoxOptions addSpecifications(FirefoxOptions options) {
+
         //TO avoid AT detection
         options.addArguments("--disable-blink-features=AutomationControlled"); // -chrome
         options.addPreference("network.proxy.type", 1);
@@ -48,34 +48,34 @@ public class Script {
         //- avoid pop ups and auto download after countdown
         options.addPreference("javascript.enabled", false);
         options.addPreference("devtools.chrome.enabled", true);
-        
-		return options;
-	}
-	
-    
-	public static void main(String[] args) throws IOException {
-    	 
+
+        return options;
+    }
+
+
+    public static void main(String[] args) throws IOException {
+
     	 //broken
       	 //String URL = "https://www.zoho.com/unknown";
-    	
+
     	 //detects AT
     	 //String URL= "https://www.zoho.com/forms/";
-    	 
+
     	 //-auto downloads 
     	 //String URL= "https://github.com/SeleniumHQ/selenium/releases/download/selenium-4.3.0/selenium-java-4.3.0.zip";
-    	 
+
     	 //-pop ups
     	 String URL = "https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwiC6qC7ifX4AhUq-jgGHamXBR8QFnoECA8QAQ&url=https%3A%2F%2Fneilpatel.com%2Fblog%2Fwebsite-popup-examples%2F&usg=AOvVaw3scgAVh9PLdPbVhYCBlBaR";
-    	
-    	 
-        System.setProperty("webdriver.gecko.driver",
-            "C:\\Automation_Tools\\Drivers\\geckodriver.exe");
-	
-        FirefoxOptions options = new FirefoxOptions();
-        //options.addArguments("--headless");
 
-        // func to add required spects
-        options = addSpecifications(options);
+
+    System.setProperty("webdriver.gecko.driver",
+        "C:\\Automation_Tools\\Drivers\\geckodriver.exe");
+	
+    FirefoxOptions options = new FirefoxOptions();
+    options.addArguments("--headless");
+
+    // func to add required spects
+    options = addSpecifications(options);
       
         WebDriver driver = new FirefoxDriver(options);
 
@@ -83,9 +83,9 @@ public class Script {
         JavascriptExecutor js = (JavascriptExecutor)driver;
        	        
         int ResponseCode = -1;
-        String ContentType="";
-      
-        try {
+        String ContentType = "";
+
+    try {
      
         URL url = new URL(URL);
         HttpURLConnection connection = (HttpURLConnection)url.openConnection();
@@ -93,64 +93,64 @@ public class Script {
         connection.connect();
 
         ResponseCode = connection.getResponseCode();
-         ContentType = connection.getHeaderField("Content-Type");
-         
-        }catch(ConnectException e){
-        	
-         System.out.println("errr while getting response on url");
-        
-        }
-        
-        System.out.println(ResponseCode);
-        System.out.println(ContentType);
+        ContentType = connection.getHeaderField("Content-Type");
 
-        switch (ResponseCode) {
-		case -1: System.out.println("Error in connecting, check Domain properly."); 
-		       return;
-		case 200: {
-			if(!ContentType.contains("text/html")) {
-				 System.out.println("Link is download link");
-				 return;
-			}	    
-		}
-		break;
-		default:
-			System.out.println("Link is broken/improper, Response code - "+ResponseCode );
-			return;
-		} 
-        
-         //- get valid url using driver
-        driver.get(URL);
-       
-        driver.manage().window().maximize();
-        driver.manage().timeouts().setScriptTimeout(5, TimeUnit.SECONDS);
-        
-        System.out.println("page loaded");
+    } catch (ConnectException e) {
+
+        System.out.println("errr while getting response on url");
+
+    }
+
+    System.out.println(ResponseCode);
+    System.out.println(ContentType);
+
+    switch (ResponseCode) {
+        case -1: System.out.println("Error in connecting, check Domain properly.");
+            return;
+        case 200: {
+            if (!ContentType.contains("text/html")) {
+                System.out.println("Link is download link");
+                return;
+            }
+        }
+            break;
+        default:
+            System.out.println("Link is broken/improper, Response code - " + ResponseCode);
+            return;
+    }
+
+    //- get valid url using driver
+    driver.get(URL);
+
+    driver.manage().window().maximize();
+    driver.manage().timeouts().setScriptTimeout(5, TimeUnit.SECONDS);
+
+    System.out.println("page loaded");
 
         //js.executeScript("alert('Welcome to 2');");
         //js.executeScript("console.log('2scds32');");
 	   
 	    String data = driver.getPageSource();
 
-        System.out.println(data.length());
+    System.out.println(data.length());
        
 		Path path
-            = Paths.get("C:\\Users\\donga\\eclipse-workspace\\Seleniumm\\domContent.html");
+        = Paths.get("C:\\Users\\donga\\eclipse-workspace\\Seleniumm\\Result\\domContent.html");
 
-        byte[] arr = data.getBytes();
+    byte[] arr = data.getBytes();
 
-        try {
+    try {
 
-            Files.write(path, arr);
-            System.out.print("Dom fetched");
-        }
-        catch (IOException ex) {
-            System.out.print("Invalid Path");
-        }
-
-        driver.quit();
-        
+        Files.write(path, arr);
+        System.out.print("Dom fetched");
     }
+    catch (IOException ex) {
+        System.out.print("Invalid Path");
+    }
+
+    driver.quit();
+
+}
 
     //-	WebDriver driver= new FirefoxDriver();	
     //- JavascriptExecutor js = (JavascriptExecutor)driver;		
